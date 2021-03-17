119a298d60c3dcf053994cb15492e8f5ea46303b
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-17 19:15:39 +0700

    initial project

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+
+func main() {
+	router := gin.Default()
+
+	router.Use(cors.Default())
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "success",
+			"message": "Welcome my application",
+		})
+	})
+
+	router.Run()
+}
