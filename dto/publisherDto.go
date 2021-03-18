fc759536bfa5c8d7ab152c578996cb78d6a90f73
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-18 08:29:53 +0700

    dto create publisher

---
+++ dto/publisherDto.go
@@ -0,0 +1,9 @@
+package dto
+
+//CreatePublisher is used when client post
+type CreatePublisher struct {
+	Name     	string `json:"name" form:"name" binding:"required"`
+	Position    string `json:"position" form:"position" binding:"required"`
+	Email    	string `json:"email" form:"email" binding:"required,email" `
+	Password 	string `json:"password" form:"password" binding:"required"`
+}
