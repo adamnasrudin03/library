f9e8cd72f6210c0e5a091543af3b94517378ab47
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-18 13:49:51 +0700

    dto UpdatePublisher

--- dto/publisherDto.go
+++ dto/publisherDto.go
@@ -13,3 +13,12 @@ type LoginPublisher struct {
 	Email    	string `json:"email" form:"email" binding:"required,email" `
 	Password 	string `json:"password" form:"password" binding:"required"`
 }
+
+//UpdatePublisher is used when client post
+type UpdatePublisher struct {
+	ID      	uint64 `json:"id" form:"id"`
+	Name     	string `json:"name" form:"name" binding:"required"`
+	Position    string `json:"position" form:"position" binding:"required"`
+	Email    	string `json:"email" form:"email" binding:"required,email" `
+	Password 	string `json:"password" form:"password" binding:"required"`
+}
