7430671d9f020001ee79344ce35834b1154482f6
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-20 15:04:39 +0700

    dto update book

--- dto/bookDto.go
+++ dto/bookDto.go
@@ -10,3 +10,14 @@ type CreateBook struct {
 	Publisher		entity.Publisher
 }
 
+
+//UpdateBook is used when client post
+type UpdateBook struct {
+	Name     		string 		`json:"name" form:"name" binding:"required"`
+	Author     		string 		`json:"author" form:"author" binding:"required"`
+	InitialStock    uint32  	`json:"initial_stock" form:"initial_stock" binding:"required"`
+	CurrentStock   	uint32  	`json:"current_stock" form:"current_stock" binding:"required"`
+	TotalBorrowers  uint32  	`json:"total_borrowers" form:"total_borrowers" binding:"required"`
+	Avaliable    	int		  	`json:"avaliable" form:"avaliable" binding:"required"`
+	Publisher		entity.Publisher
+}
