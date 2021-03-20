2861d25f01f4c4c93da47e85ce02abfcbe83e257
Adam Nasrudin <adamnasrudin858@gmail.com>
2021-03-20 09:29:59 +0700

    book service method create book

--- service/bookService.go
+++ service/bookService.go
@@ -1,8 +1,14 @@
 package service
 
-import "github.com/adamnasrudin03/library/repository"
+import (
+	"github.com/adamnasrudin03/library/dto"
+	"github.com/adamnasrudin03/library/entity"
+	"github.com/adamnasrudin03/library/repository"
+)
+
 
 type BookService interface {
+	CreateBook(input dto.CreateBook) (entity.Book, error)
 }
 
 type bookService struct {
@@ -11,4 +17,22 @@ type bookService struct {
 
 func NewBookService(repository repository.BookRepository) *bookService {
 	return &bookService{repository}
+}
+
+func (s *bookService) CreateBook(input dto.CreateBook) (entity.Book, error) {
+	book := entity.Book{}
+	book.Name = input.Name
+	book.Author = input.Author
+	book.InitialStock = input.InitialStock
+	book.CurrentStock = input.InitialStock
+	book.TotalBorrowers = 0
+	book.Avaliable = 1
+	book.PublisherID = input.Publisher.ID
+
+	newBook, err := s.repository.Save(book)
+	if err != nil {
+		return newBook, err
+	}
+
+	return newBook, nil
 }
